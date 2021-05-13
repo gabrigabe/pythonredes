465b660f1fb408a87db968332255fe4b4b3cf61b
Gabriel Bezerra <gabrielbrodrigues1@gmail.com>
2021-05-13 13:57:39 -0300

    Teste de receiver 2.1 provavelmente não funcional

--- trasmissor.py
+++ trasmissor.py
@@ -1,41 +1,49 @@
 import socket
-socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+import numpy as np  # pip install numpy
+
+
 socketUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-transmissor = ("localhost", 2020)
-receptor = ("localhost", 3030)
-buff_size = 1024
-socket.bind(transmissor)
+transmissor = ("127.0.0.1", 2020)
+receptor = ("127.0.0.1", 3030)
+socketUDP.bind(transmissor)
+buff_size = 10000
+
+next_sequence_number = 0
+
 
 def calculate_checksum(data):
-    data_sum = 0
+    data_sum = np.uint16(0)
     for element in data:
-        data_sum +=element
-    return ~data_sum
+        data_sum += element
+    return np.invert(data_sum)
 
-def verify_checksum(checksum, data):
-   data_sum = 0
+
+def verify_checksum(data):
+    data_sum = np.uint16(0)
     for element in data:
-      data_sum +=element
-    return ~data_sum
+        data_sum += element
+    return data_sum == 0xFFFF
 
 
 def udt_send(packet):
-    socketUDP.sendto(packet, receptor)   
+    print(packet)
+    socketUDP.sendto(packet.tobytes(), receptor)
+
 
 def rdt_rcv():
-     while True:
-        message, source = socket.recvfrom(buff_size)
+    while True:
+        message, source = socketUDP.recvfrom(buff_size)
         if source == receptor:
-            return message
+            return np.frombuffer(message, dtype=np.uint16)
+
 
 def rdt_send(data):
-    packet = []
-    packet.append(0)
-    packet.append(calculate_checksum(data   ))
-    packet.extend(data)
+    global next_sequence_number
 
-    udt_send(packet)
+    sndpkt = np.array([], np.uint16)
+    sndpkt = np.append(sndpkt, np.uint16(next_sequence_number))
+    sndpkt = np.append(sndpkt, np.uint16(0))  # checksum
+    sndpkt = np.concatenate((sndpkt, data))
 
-    rcvpkt = rdt_rcv()
-    checksum = rcvpkt[1]
-    if verify_checksum(checksum, data)
+    sndpkt[1] = calculate_checksum(sndpkt)
+    udt_send(sndpkt)
